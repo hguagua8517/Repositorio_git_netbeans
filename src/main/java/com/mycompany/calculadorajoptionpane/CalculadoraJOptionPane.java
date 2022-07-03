d2b643153f29e4146977397dfefdb29d1089d42b
hguagua8517 <guaguaso9@yahoo.com>
2022-07-02 19:09:59 -0500

    archivo de prueba

---
+++ src/main/java/com/mycompany/calculadorajoptionpane/CalculadoraJOptionPane.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.calculadorajoptionpane;
+
+import OperacionesBasicas.OperacionesBasicas;
+import java.lang.NullPointerException;
+
+
+/**
+ *
+ * @author Usuario
+ */
+public class CalculadoraJOptionPane {
+
+    public static void main(String[] args) {
+        OperacionesBasicas operaciones = new OperacionesBasicas(); 
+        operaciones.opcionesIniciales();
+    }
+    
+}
